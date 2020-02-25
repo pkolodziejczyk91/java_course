2ef0d7a75f382cd4b66119d9200248b6b4a300ff
pkolodziejczyk91 <61356091+pkolodziejczyk91@users.noreply.github.com>
2020-02-25 23:27:36 +0100

    Project changes

--- sandbox/src/main/java/piotr/javaCourse/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/piotr/javaCourse/sandbox/MyFirstProgram.java
@@ -3,8 +3,29 @@ package piotr.javaCourse.sandbox;
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
+		hello("world");
+		hello("user");
+		hello("Piotr");
 
-		System.out.println("Hello, world!");
+		double l = 5;
+		System.out.println("Powierzchnia kwadratu o boku " + l + " = " + area(l));
+
+		double a = 4;
+		double b = 6;
+		System.out.println("Powierzchnia prostokąta o bokach " + a + " i " + b + " = " + area(a, b));
+
+	}
+
+	public static void hello(String somebody) {
+		System.out.println("Hello, " + somebody + "!");
+
+	}
+	public static double area(double len) {
+		return len * len;
+	}
+
+	public static double area (double a, double b) {
+		return a * b;
 	}
 
 }
