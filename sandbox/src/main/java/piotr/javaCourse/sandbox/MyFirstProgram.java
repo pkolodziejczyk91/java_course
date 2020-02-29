4640da41b0b553b4bed95fe90f3e49ed5e1063f2
pkolodziejczyk91 <61356091+pkolodziejczyk91@users.noreply.github.com>
2020-02-29 12:10:32 +0100

    TestNG applying and first square test

--- sandbox/src/main/java/piotr/javaCourse/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/piotr/javaCourse/sandbox/MyFirstProgram.java
@@ -7,25 +7,17 @@ public class MyFirstProgram {
 		hello("user");
 		hello("Piotr");
 
-		double l = 5;
-		System.out.println("Powierzchnia kwadratu o boku " + l + " = " + area(l));
+		Square s = new Square(5);
+		System.out.println("Powierzchnia kwadratu o boku " + s.l + " = " + s.area());
 
-		double a = 4;
-		double b = 6;
-		System.out.println("Powierzchnia prostokąta o bokach " + a + " i " + b + " = " + area(a, b));
+		Rectangle r = new Rectangle(4, 6);
+		System.out.println("Powierzchnia prostokąta o bokach " + r.a + " i " + r.b + " = " + r.area());
 
 	}
 
 	public static void hello(String somebody) {
 		System.out.println("Hello, " + somebody + "!");
 
-	}
-	public static double area(double len) {
-		return len * len;
-	}
-
-	public static double area (double a, double b) {
-		return a * b;
 	}
 
 }
