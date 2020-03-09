cf29d7a1046aaa80bb12058b21431a8673efeeb3
pkolodziejczyk91 <61356091+pkolodziejczyk91@users.noreply.github.com>
2020-03-09 21:43:41 +0100

    Implemented test execution with preconditions

--- addressbook-web-tests/src/test/java/piotr/javaCourse/addressBook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/piotr/javaCourse/addressBook/tests/GroupDeletionTests.java
@@ -1,12 +1,16 @@
 package piotr.javaCourse.addressBook.tests;
 
 import org.testng.annotations.*;
+import piotr.javaCourse.addressBook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
   @Test
   public void testGroupDeletion() {
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
