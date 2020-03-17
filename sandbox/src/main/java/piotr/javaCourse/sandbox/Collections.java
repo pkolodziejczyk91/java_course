7dd5cfb134fcad0c36537513bacfb14f688b32f9
pkolodziejczyk91 <61356091+pkolodziejczyk91@users.noreply.github.com>
2020-03-17 16:59:26 +0100

    Create Collections

---
+++ sandbox/src/main/java/piotr/javaCourse/sandbox/Collections.java
@@ -0,0 +1,16 @@
+package piotr.javaCourse.sandbox;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Collections {
+  public static void main(String[] args) {
+    String[] langs = {"Java", "C#", "Python", "PHP"};
+
+    List<String> languages = Arrays.asList("Java", "C#", "Python", "PHP");
+
+    for (String l : languages) {
+      System.out.println("i want to learn " + l);
+    }
+  }
+}
