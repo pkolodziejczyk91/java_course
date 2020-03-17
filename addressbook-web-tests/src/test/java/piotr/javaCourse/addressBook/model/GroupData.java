3026181a7f994f5da538a115023e23d4933cd7aa
pkolodziejczyk91 <61356091+pkolodziejczyk91@users.noreply.github.com>
2020-03-17 19:42:03 +0100

    Group collection comparision

--- addressbook-web-tests/src/test/java/piotr/javaCourse/addressBook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/piotr/javaCourse/addressBook/model/GroupData.java
@@ -1,9 +1,31 @@
 package piotr.javaCourse.addressBook.model;
 
+import java.util.Objects;
+
 public class GroupData {
   private final String name;
   private final String header;
   private final String footer;
+
+  @Override
+  public String toString() {
+    return "GroupData{" +
+            "name='" + name + '\'' +
+            '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData groupData = (GroupData) o;
+    return Objects.equals(name, groupData.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name);
+  }
 
   public GroupData(String name, String header, String footer) {
     this.name = name;
--- addressbook-web-tests/src/test/java/piotr/javaCourse/addressBook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/piotr/javaCourse/addressBook/tests/GroupDeletionTests.java
@@ -19,7 +19,9 @@ public class GroupDeletionTests extends TestBase {
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
-    Assert.assertEquals(after.size(), before.size() +1);
-  }
+    Assert.assertEquals(after.size(), before.size() -1);
 
+    before.remove(before.size() -1);
+      Assert.assertEquals(before, after);
+    }
 }
