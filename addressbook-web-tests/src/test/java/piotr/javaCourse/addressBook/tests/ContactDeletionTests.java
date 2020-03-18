2e8196a008c18989930355b73c60b10dc95b9f65
pkolodziejczyk91 <61356091+pkolodziejczyk91@users.noreply.github.com>
2020-03-18 20:42:53 +0100

    Exercise 10

--- addressbook-web-tests/src/test/java/piotr/javaCourse/addressBook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/piotr/javaCourse/addressBook/tests/ContactDeletionTests.java
@@ -1,7 +1,11 @@
 package piotr.javaCourse.addressBook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import piotr.javaCourse.addressBook.model.ContactData;
+
+
+import java.util.List;
 
 public class ContactDeletionTests extends TestBase {
 
@@ -9,10 +13,18 @@ public class ContactDeletionTests extends TestBase {
 
   public void testContactDeletion() {
     if (! app.getContactHelper().isThereAContact()) {
-      app.getContactHelper().createContact(new ContactData("Piotr", "Michał", "Kołodziejczyk", "Mr.", "pkolodziejczyk91@gmail.com", "111 111 111", "test1"));
+      app.getContactHelper().createContact(new ContactData("Piotr", "Kołodziejczyk"));
     }
-    app.getContactHelper().selectContact();
+    List<ContactData> before = app.getContactHelper().getContactList();
+    app.getContactHelper().selectContact(before.size() -1);
     app.getContactHelper().deleteContact();
     app.getContactHelper().acceptContactAlert();
+    app.getNavigationHelper().returnToHomePage();
+    List<ContactData> after = app.getContactHelper().getContactList();
+
+    Assert.assertEquals(after.size(), before.size() -1);
+
+    before.remove(before.size() -1);
+    Assert.assertEquals(before, after);
   }
 }
