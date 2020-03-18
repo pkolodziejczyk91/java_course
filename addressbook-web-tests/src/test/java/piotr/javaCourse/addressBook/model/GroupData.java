d1e2bedcca25c200097765e3b4042745b06133ac
pkolodziejczyk91 <61356091+pkolodziejczyk91@users.noreply.github.com>
2020-03-18 17:07:51 +0100

    Unordered collections and finding maximum values

--- addressbook-web-tests/src/test/java/piotr/javaCourse/addressBook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/piotr/javaCourse/addressBook/model/GroupData.java
@@ -3,31 +3,21 @@ package piotr.javaCourse.addressBook.model;
 import java.util.Objects;
 
 public class GroupData {
+  private int id;
   private final String name;
   private final String header;
   private final String footer;
 
-  @Override
-  public String toString() {
-    return "GroupData{" +
-            "name='" + name + '\'' +
-            '}';
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    GroupData groupData = (GroupData) o;
-    return Objects.equals(name, groupData.name);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(name);
-  }
 
   public GroupData(String name, String header, String footer) {
+    this.id = 0;
+    this.name = name;
+    this.header = header;
+    this.footer = footer;
+  }
+
+  public GroupData(int id, String name, String header, String footer) {
+    this.id = id;
     this.name = name;
     this.header = header;
     this.footer = footer;
@@ -43,5 +33,33 @@ public class GroupData {
 
   public String getFooter() {
     return footer;
+  }
+
+  public int getId() { return id; }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  @Override
+  public String toString() {
+    return "GroupData{" +
+            "id='" + id + '\'' +
+            ", name='" + name + '\'' +
+            '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData groupData = (GroupData) o;
+    return id == groupData.id &&
+            Objects.equals(name, groupData.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, name);
   }
 }
