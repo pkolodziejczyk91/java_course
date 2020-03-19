34cd148268fb4bda8206eb7e6eb82ce5483595d8
pkolodziejczyk91 <61356091+pkolodziejczyk91@users.noreply.github.com>
2020-03-19 11:57:12 +0100

    Revert "Exercise 10"
    This reverts commit 2e8196a008c18989930355b73c60b10dc95b9f65.

--- addressbook-web-tests/src/test/java/piotr/javaCourse/addressBook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/piotr/javaCourse/addressBook/tests/ContactCreationTests.java
@@ -1,22 +1,15 @@
 package piotr.javaCourse.addressBook.tests;
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import piotr.javaCourse.addressBook.model.ContactData;
-
-import java.util.List;
 
 public class ContactCreationTests extends TestBase {
 
   @Test
   public void testContactCreation() {
-    List<ContactData> before = app.getContactHelper().getContactList();
     app.getContactHelper().initContactCreation();
-    app.getContactHelper().fillContactForm(new ContactData("Piotr", "Kołodziejczyk"), false);
+    app.getContactHelper().fillContactForm(new ContactData("Piotr", "Michał", "Kołodziejczyk", "Mr.", "pkolodziejczyk91@gmail.com", "111 111 111", "test1"), true);
     app.getContactHelper().submitContactCreation();
     app.getNavigationHelper().returnToHomePage();
-    List<ContactData> after = app.getContactHelper().getContactList();
-
-    Assert.assertEquals(after.size(), before.size() +1);
   }
 }
--- addressbook-web-tests/src/test/java/piotr/javaCourse/addressBook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/piotr/javaCourse/addressBook/tests/ContactModificationTests.java
@@ -1,11 +1,7 @@
 package piotr.javaCourse.addressBook.tests;
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import piotr.javaCourse.addressBook.model.ContactData;
-
-import java.util.HashSet;
-import java.util.List;
 
 public class ContactModificationTests extends TestBase {
 
@@ -13,21 +9,11 @@ public class ContactModificationTests extends TestBase {
 
   public void testContactModification() {
     if (! app.getContactHelper().isThereAContact()) {
-      app.getContactHelper().createContact(new ContactData(null, "Piotr","Kołodziejczyk"));
+      app.getContactHelper().createContact(new ContactData("Piotr", "Michał", "Kołodziejczyk", "Mr.", "pkolodziejczyk91@gmail.com", "111 111 111", "test1"));
     }
-    List<ContactData> before = app.getContactHelper().getContactList();
-    app.getContactHelper().selectContact(before.size() -1);
     app.getContactHelper().initContactModification();
-    ContactData contact = new ContactData("Piotr", "Kołodziejczyk");
-    app.getContactHelper().fillContactForm(contact, false);
+    app.getContactHelper().fillContactForm(new ContactData("Michal", "Piotr", "Kołodziejczyk", "Mr.", "pkolodziejczyk91@gmail.com", "111 111 111", null), false);
     app.getContactHelper().submitContactModification();
     app.getNavigationHelper().returnToHomePage();
-    List<ContactData> after = app.getContactHelper().getContactList();
-
-    Assert.assertEquals(after.size(), before.size());
-
-    before.remove(before.size() -1);
-    before.add(contact);
-    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 }
