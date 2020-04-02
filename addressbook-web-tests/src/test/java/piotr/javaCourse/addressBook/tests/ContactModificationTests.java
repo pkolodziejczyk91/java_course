d2ae6d42dd5d0abb3b87d205d85e3ff84256f370
pkolodziejczyk91 <61356091+pkolodziejczyk91@users.noreply.github.com>
2020-04-02 16:34:36 +0200

    Revert "Revert "Revert "Revert "Exercise 10""""
    This reverts commit 53e8fec5ab1154765f43cdb8afb9534ed728f582.

--- addressbook-web-tests/src/test/java/piotr/javaCourse/addressBook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/piotr/javaCourse/addressBook/tests/ContactModificationTests.java
@@ -1,7 +1,11 @@
 package piotr.javaCourse.addressBook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import piotr.javaCourse.addressBook.model.ContactData;
+
+import java.util.HashSet;
+import java.util.List;
 
 public class ContactModificationTests extends TestBase {
 
@@ -9,11 +13,21 @@ public class ContactModificationTests extends TestBase {
 
   public void testContactModification() {
     if (! app.getContactHelper().isThereAContact()) {
-      app.getContactHelper().createContact(new ContactData("Piotr", "Michał", "Kołodziejczyk", "Mr.", "pkolodziejczyk91@gmail.com", "111 111 111", "test1"));
+      app.getContactHelper().createContact(new ContactData(null, "Piotr","Kołodziejczyk"));
     }
+    List<ContactData> before = app.getContactHelper().getContactList();
+    app.getContactHelper().selectContact(before.size() -1);
     app.getContactHelper().initContactModification();
-    app.getContactHelper().fillContactForm(new ContactData("Michal", "Piotr", "Kołodziejczyk", "Mr.", "pkolodziejczyk91@gmail.com", "111 111 111", null), false);
+    ContactData contact = new ContactData("Piotr", "Kołodziejczyk");
+    app.getContactHelper().fillContactForm(contact, false);
     app.getContactHelper().submitContactModification();
     app.getNavigationHelper().returnToHomePage();
+    List<ContactData> after = app.getContactHelper().getContactList();
+
+    Assert.assertEquals(after.size(), before.size());
+
+    before.remove(before.size() -1);
+    before.add(contact);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 }
