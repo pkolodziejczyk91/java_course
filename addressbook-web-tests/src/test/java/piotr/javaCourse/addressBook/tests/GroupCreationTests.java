6056aa222e4d7647e0b7f322df6bde7bc7029192
pkolodziejczyk91 <61356091+pkolodziejczyk91@users.noreply.github.com>
2020-04-22 20:30:28 +0200

    Cashing and Hashing for Groups

--- addressbook-web-tests/src/test/java/piotr/javaCourse/addressBook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/piotr/javaCourse/addressBook/tests/GroupCreationTests.java
@@ -1,19 +1,10 @@
 package piotr.javaCourse.addressBook.tests;
 
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.MatcherAssert;
-import org.testng.Assert;
 import org.testng.annotations.*;
 import piotr.javaCourse.addressBook.model.GroupData;
 import piotr.javaCourse.addressBook.model.Groups;
-
-import java.util.Comparator;
-import java.util.List;
-import java.util.Set;
-
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.testng.Assert.assertEquals;
 
 
 public class GroupCreationTests extends TestBase {
@@ -24,10 +15,20 @@ public class GroupCreationTests extends TestBase {
     Groups before = app.group().all();
     GroupData group = new GroupData().withName("test2");
     app.group().create(group);
+    assertThat(app.group().count(), equalTo(before.size() +1));
     Groups after = app.group().all();
-    assertThat(after.size(), equalTo(before.size() +1));
-
     assertThat(after, equalTo(
             before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
+
+  @Test
+  public void testBadGroupCreation() {
+    app.goTo().groupPage();
+    Groups before = app.group().all();
+    GroupData group = new GroupData().withName("test'");
+    app.group().create(group);
+    assertThat(app.group().count(), equalTo(before.size()));
+    Groups after = app.group().all();
+    assertThat(after.size(), equalTo(before));
+}
 }
--- addressbook-web-tests/src/test/java/piotr/javaCourse/addressBook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/piotr/javaCourse/addressBook/tests/GroupDeletionTests.java
@@ -1,15 +1,8 @@
 package piotr.javaCourse.addressBook.tests;
 
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.MatcherAssert;
-import org.testng.Assert;
 import org.testng.annotations.*;
 import piotr.javaCourse.addressBook.model.GroupData;
 import piotr.javaCourse.addressBook.model.Groups;
-
-import java.util.List;
-import java.util.Set;
-
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.testng.Assert.assertEquals;
@@ -29,8 +22,8 @@ public class GroupDeletionTests extends TestBase {
     Groups before = app.group().all();
     GroupData deletedGroup = before.iterator().next();
     app.group().delete(deletedGroup);
+    assertThat(app.group().count(), equalTo(before.size() -1));
     Groups after = app.group().all();
-    assertEquals(after.size(), before.size() -1);
     assertThat(after, equalTo(before.without(deletedGroup)));
     }
 }
