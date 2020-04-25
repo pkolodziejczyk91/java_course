4bdbea5c3c8de0e81b63a9f75956f25f43a69993
pkolodziejczyk91 <61356091+pkolodziejczyk91@users.noreply.github.com>
2020-04-25 13:50:45 +0200

    Test stand configuration

--- addressbook-web-tests/src/test/java/piotr/javaCourse/addressBook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/piotr/javaCourse/addressBook/appmanager/ApplicationManager.java
@@ -8,9 +8,12 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.*;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
+  private final Properties properties;
   WebDriver wd;
 
   private SessionHelper sessionHelper;
@@ -19,11 +22,15 @@ public class ApplicationManager {
   private ContactHelper contactHelper;
   private String browser;
 
-  public ApplicationManager(String browser) {
+  public ApplicationManager(String browser) throws IOException {
     this.browser = browser;
+    properties = new Properties();
   }
 
-  public void init() {
+  public void init() throws IOException {
+    String target = System.getProperty("target", "local");
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
     if (browser.equals(BrowserType.FIREFOX)) {
       wd = new FirefoxDriver();
     } else if (browser.equals(BrowserType.CHROME)) {
@@ -33,12 +40,12 @@ public class ApplicationManager {
     }
 
     wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-    wd.get("http://localhost/addressbook/");
+    wd.get(properties.getProperty("webBaseUrl"));
     groupHelper = new GroupHelper(wd);
     navigationHelper = new NavigationHelper(wd);
     sessionHelper = new SessionHelper(wd);
     contactHelper = new ContactHelper(wd);
-    sessionHelper.login("admin", "secret");
+    sessionHelper.login(properties.getProperty("webAdminLogin"), properties.getProperty("webAdminPassword"));
   }
 
   public void stop() { wd.quit();
--- addressbook-web-tests/src/test/java/piotr/javaCourse/addressBook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/piotr/javaCourse/addressBook/tests/TestBase.java
@@ -7,7 +7,8 @@ import piotr.javaCourse.addressBook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+  protected static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite(alwaysRun = true)
   public void setUp()  {
